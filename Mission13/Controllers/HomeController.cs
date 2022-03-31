a96d3269983374e4d923f2f751e716c1afd08591
thomasblackwelder <83183821+thomasblackwelder@users.noreply.github.com>
2022-03-30 23:50:49 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        public HomeController(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+
+            var blah = _repo.Bowlers
+                .Include(x => x.Team)
+                .ToList();
+
+            return View(blah);
+        }
+
+        
+    }
+}
